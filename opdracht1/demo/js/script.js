f40afa24358cee5d7044eaad22010f14ab92d60b
JustinLindhout <57796356+JustinLindhout@users.noreply.github.com>
2020-05-14 15:14:58 +0200

    update

--- opdracht1/demo/js/script.js
+++ opdracht1/demo/js/script.js
@@ -4,3 +4,14 @@
   2. Koppel een evenListener aan het element om een mouse-event te detecteren
   3. Gebruik het Classlist object om een css class aan een element toe te voegen of weg te halen.
 */
+
+var blue = document.querySelectorAll("img.blue");
+var green = document.querySelectorAll("img.green");
+var yellow = document.querySelectorAll("img.yellow");
+var red = document.querySelectorAll("img.red");
+
+var button = document.querySelector("btnBlue");
+
+btnBlue.addEventListener("click", function () {
+			element.classList.toggle("greenhide", "yellowhide", "redhide");
+}
